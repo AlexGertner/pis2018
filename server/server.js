110f9d1df2b10244d8f988481158125c776ed5da
AlexGertner <alexander.gaertner96@gmail.com>
2018-07-04 06:34:39 +0200

    backup02

--- server/server.js
+++ server/server.js
@@ -8,6 +8,23 @@ const express = require("express");
 /* socket connection */
 const socket = require("socket.io");
 const firebase = require("firebase");
+/*const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+});
+
+let db = admin.firestore();*/
+let config = {
+    apiKey: 'AIzaSyC98zadrb1VmdtNOPkCDxECJ9b2PODCSRY',
+    authDomain: 'chat-app-67395.firebaseapp.com',
+    databaseURL: 'https://chat-app-67395.firebaseio.com',
+    projectId: 'chat-app-67395',
+    storageBucket: 'chat-app-67395.appspot.com',
+    messagingSenderId: '572008863146'
+};
+firebase.initializeApp(config);
+let database = firebase.database();
 /*****************************************************************************
  ***  Class Client                                                           *
  *****************************************************************************/
@@ -18,7 +35,6 @@ class Client {
         this.username = username;
     }
 }
-let database = firebase.database();
 let router = express();
 // router.listen(8080);
 //let server = https.createServer(credentials, router).listen(4200);
@@ -55,20 +71,54 @@ io.on('connection', (socket) => {
     });
     //listens on "chat"-emit to then emit the received data back to the client
     socket.on('chat', function (data) {
-        let socketID;
-        for (let i = 0; i < clientList.length; i++) {
-            if (data.to == clientList[i].username) {
-                socketID = clientList[i].socketID;
+        clientList.forEach((client) => {
+            if (data.from == client.username) {
+                io.sockets.connected[client.socketID].emit('chat', data);
             }
-        }
-        console.log(socketID);
+            if (data.to == client.username) {
+                io.sockets.connected[client.socketID].emit('chat', data);
+            }
+        });
         //storeChatToFirebase(data);
-        io.sockets.connected[socketID].emit('chat', data);
+    });
+    socket.on('storeChat', function (chat) {
+        storeChatToFirebase(chat);
+    });
+    socket.on('getChat', function (item) {
+        console.log("getChat ausgelöst");
+        firebase.database().ref('/chats/' + item).once('value', function (snapshot) {
+            let returnArr = [];
+            snapshot.forEach(function (childSnapshot) {
+                let item = childSnapshot.val();
+                returnArr.push(item);
+            });
+            socket.emit('takeChat', returnArr);
+        });
     });
 });
 function storeChatToFirebase(data) {
-    let dataRef = this.af.database.list('/chats/' + this.currentUser.company);
-    return dataRef.push(data);
+    firebase.database().ref("/chats/" + data.from).push({
+        to: data.to,
+        message: data.message
+    });
+    /*let docRef = db.collection(data.from).doc(data.to);
+
+    let setChat = docRef.set({
+        message: data.message,
+    });*/
+    //return Promise.all([setChat]);
+}
+function getChatFromFirebase(item) {
+    console.log("getChatFromFirebase ausgelöst");
+    firebase.database().ref('/chats/' + item).on('value', function (snapshot) {
+        let returnArr = [];
+        snapshot.forEach(function (childSnapshot) {
+            let item = childSnapshot.val();
+            returnArr.push(item);
+        });
+        console.log(returnArr);
+        return returnArr;
+    });
 }
 /*//--- static routes -----------------------------------------------------------
 router.use("/client", express.static(clientPath));
